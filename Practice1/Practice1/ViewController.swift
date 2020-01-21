df56af34de6475080bd595056f810e2ae5dee9a7
forestcrasher <forestcrasher@outlook.com>
2020-01-21 20:23:07 +0300

    Start new project

---
+++ Practice1/Practice1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice1
+//
+//  Created by Антон Пряхин on 21.01.2020.
+//  Copyright © 2020 Антон Пряхин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
