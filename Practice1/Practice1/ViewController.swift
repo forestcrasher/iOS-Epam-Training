a2dfd79abfbd1c20b003bc373b140cddebebb3a5
forestcrasher <forestcrasher@outlook.com>
2020-01-26 02:30:02 +0300

    Create first application

--- Practice1/Practice1/ViewController.swift
+++ Practice1/Practice1/ViewController.swift
@@ -2,19 +2,70 @@
 //  ViewController.swift
 //  Practice1
 //
-//  Created by Антон Пряхин on 21.01.2020.
-//  Copyright © 2020 Антон Пряхин. All rights reserved.
+//  Created by Anton Pryakhin on 21.01.2020.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    var randomNumber = 0
+    var isWinner = false
 
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var answerField: UITextField!
+    @IBOutlet weak var checkButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startGame()
     }
-
-
+    
+    func startGame() {
+        setRandomNumber()
+        checkButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+    }
+    
+    func stopGame() {
+        statusLabel.text = "Угадал"
+        checkButton.setTitle("Заново", for: .normal)
+        answerField.isUserInteractionEnabled = false
+        isWinner = true
+    }
+    
+    func restartGame() {
+        setRandomNumber()
+        statusLabel.text = ""
+        checkButton.setTitle("Проверить", for: .normal)
+        answerField.isUserInteractionEnabled = true
+        isWinner = false
+    }
+    
+    func setRandomNumber() {
+        randomNumber = Int.random(in: 0 ..< 100)
+    }
+    
+    func checkAnswer() {
+        if let answer = answerField.text {
+            if let answerNumber = Int(answer) {
+                if answerNumber > randomNumber {
+                    statusLabel.text = "Много"
+                } else if answerNumber < randomNumber {
+                    statusLabel.text = "Мало"
+                } else {
+                    stopGame()
+                }
+            } else {
+                statusLabel.text = "Введите число"
+            }
+        }
+    }
+    
+    @IBAction func buttonAction() {
+        if isWinner {
+            restartGame()
+        } else {
+            checkAnswer()
+        }
+    }
 }
 
